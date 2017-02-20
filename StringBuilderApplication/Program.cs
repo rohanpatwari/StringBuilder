c51019486ca8c1e9d44c9bce381c81c4be81f2a5
rohan <rohan@DESKTOP-2911EVS>
2017-02-20 14:52:23 -0600

    Add project files.

---
+++ StringBuilderApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilderApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Welcome to .Net tutorial.list of topics include:");//Append
+            sb.Remove(25, 7);//Remove
+            sb.AppendLine();//AppendLine
+            sb.Append("C#.Net, ASP.Net, ADO.Net, MVC").AppendLine();
+            Console.WriteLine(sb.ToString());
+            Console.ReadLine();
+        }
+    }
+}
